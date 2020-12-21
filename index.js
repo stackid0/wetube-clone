655e02989425b3e06b01f7a4271ab783dbb4c335
stackid0 <73723984+stackid0@users.noreply.github.com>
2020-12-21 18:22:06 +0900

    Route and Middleware

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("메인 URL에 오신 것을 환영합니다!");
+
+const handleProfile = (req, res) => res.send("프로필 화면");
+
+const betweenHome = (req, res, next) => {
+    console.log("/ 거쳐가는 중...");
+    next();
 }
+
+app.use(betweenHome);
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
